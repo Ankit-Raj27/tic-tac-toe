7add6385bdfc92a769c9be04cd4da6c16784f1a1
Ankit Raj <ankitrajk27@gmail.com>
2022-04-21 18:42:18 +0530

    fix

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const checkWin = ()=>
     wins.forEach(e =>{
         if(boxtext[e[0]].innerText===boxtext[e[1]].innerText && (boxtext[e[2]].innerText ===boxtext[e[1]].innerText) && (boxtext[e[1]].innerText !==""))
         {
-            document.querySelector(".info").innerText="Ankit the GOD has WON!!";
+            document.querySelector(".info").innerText=boxtext[e[0]].innerText + " has WON!! but the real winner is ANKIT";
             isgameover = true;
             document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "200px";
         }
