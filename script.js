75d74defec5f28100c694d0a8c91f1e0e9262834
Ankit Raj <ankitrajk27@gmail.com>
2022-04-21 18:23:47 +0530

    Completed the prototype

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 console.log("Welcome to My TicTacToe!");
-let music = new Audio("music.mp3");
+
 let ting = new Audio("ting.mp3");
 let gameover = new Audio("gameover.mp3");
 let turn = "X";
+let isgameover = false;
 
 // Function to change the player's turn
 
@@ -13,8 +14,27 @@ const changeTurn = () =>
 
 
 //  Function to check for Win
-const checkWin = ()=>{
-
+const checkWin = ()=>
+{
+    let boxtext = document.getElementsByClassName("boxtext");
+    let wins = [
+        [0,1,2],
+        [0,3,6],
+        [3,4,5],
+        [1,4,7],
+        [6,7,8],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ]
+    wins.forEach(e =>{
+        if(boxtext[e[0]].innerText===boxtext[e[1]].innerText && (boxtext[e[2]].innerText ===boxtext[e[1]].innerText) && (boxtext[e[1]].innerText !==""))
+        {
+            document.querySelector(".info").innerText=boxtext[e[0]].innerText + " has WON!!";
+            isgameover = true;
+            document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "200px";
+        }
+    });
 }
 
 // Game Logic
@@ -29,7 +49,22 @@ Array.from(boxes).forEach(element =>
                 turn =  changeTurn();
                 ting.play();
                 checkWin();
+                if(!isgameover){
                 document.getElementsByClassName("info")[0].innerText = "Turn for " + turn; 
+                }
             }
         })
+    })
+
+    // Add onclick listener to reset butotn
+
+    reset.addEventListener("click", ()=>{
+        let boxtexts = document.querySelectorAll(".boxtext")
+        Array.from(boxtexts).forEach(element =>{
+            element.innerText = ""
+        })
+        turn = "X"
+        isgameover = false;
+        document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+        document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "0px";
     })
